c3a0587eceb86b7107466e03b8350790a834a81d
Tom Grimwood <tgri417@aucklanduni.ac.nz>
2021-08-09 13:44:46 +1200

    Not Working Circle Attempt

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,19 @@
 #include <Encoder.h>
 #include <PID_v1.h>
 #include <functions.hpp>
-#define CENTIMETRE 1852
+#define CENTIMETRE 1852 //encoder count for 1cm
 #define SQUARE_SIZE 3
+#define CIRCLE_SIZE .6 //in centimetres
 #define ROTATION 5000
 #define SAMPLE_TIME 10 //minimum time till PID recalculates  the  output (MilliSeconds)
 
 double Setpoint, Input, Output;
+double SetpointR, InputR, OutputR;
+
+double thisSin;
+double thisCos;
+double totalMovedX = 0;
+double totalMovedY = 0;
 
 int E1 = 5;
 int M1 = 4;
@@ -16,11 +23,13 @@ int M2 = 7;
 bool flag = true;
 
 PID myPID(&Input, &Output, &Setpoint, 3, 0, 0.1, DIRECT);
+PID myOtherPID(&InputR, &OutputR, &SetpointR, 3, 0, 0.1, DIRECT);
 Encoder knobLeft(2, 3);
+Encoder knobRight(3, 4);
 
 void setup()
 {
-  //Serial.begin(9600);
+  Serial.begin(9600);
   pinMode(LEFT_KILL_PIN, INPUT);
   pinMode(RIGHT_KILL_PIN, INPUT);
   pinMode(UP_KILL_PIN, INPUT);
@@ -33,24 +42,25 @@ void setup()
   myPID.SetOutputLimits(-DUTY_CYCLE_LIMIT, DUTY_CYCLE_LIMIT);
   myPID.SetSampleTime(SAMPLE_TIME);
   myPID.SetControllerDirection(1);
+
+  myOtherPID.SetMode(AUTOMATIC);
+  myOtherPID.SetOutputLimits(-DUTY_CYCLE_LIMIT, DUTY_CYCLE_LIMIT);
+  myOtherPID.SetSampleTime(SAMPLE_TIME);
+  myOtherPID.SetControllerDirection(1);
 }
 void loop()
 {
 
-  moveDistance(SQUARE_SIZE * CENTIMETRE, HORIZONTAL);
-  moveDistance(-SQUARE_SIZE * CENTIMETRE, VERTICAL);
-  moveDistance(-SQUARE_SIZE * CENTIMETRE, HORIZONTAL);
-  moveDistance(SQUARE_SIZE * CENTIMETRE, VERTICAL);
-  delay(2000);
+  thisSin = +CIRCLE_SIZE * CENTIMETRE * sin(millis() / 10000 * 2 * PI);
 
-  moveDistance(2 * SQUARE_SIZE * CENTIMETRE, HORIZONTAL);
-  moveDistance(-2 * SQUARE_SIZE * CENTIMETRE, VERTICAL);
-  moveDistance(-2 * SQUARE_SIZE * CENTIMETRE, HORIZONTAL);
-  moveDistance(2 * SQUARE_SIZE * CENTIMETRE, VERTICAL);
-  delay(2000);
-  moveDistance(2.5 * SQUARE_SIZE * CENTIMETRE, HORIZONTAL);
-  moveDistance(2.5 * -SQUARE_SIZE * CENTIMETRE, VERTICAL);
-  moveDistance(2.5 * -SQUARE_SIZE * CENTIMETRE, HORIZONTAL);
-  moveDistance(2.5 * SQUARE_SIZE * CENTIMETRE, VERTICAL);
-  delay(2000);
+  thisCos = +CIRCLE_SIZE * CENTIMETRE * cos(millis() / 10000 * 2 * PI);
+
+  moveDistance(thisSin - totalMovedX, HORIZONTAL);
+  Serial.print("MOVED HORIZONTAL ");
+  Serial.println(thisSin - totalMovedX);
+  totalMovedX = totalMovedX + thisSin;                              
+  moveDistance(thisCos - totalMovedY, VERTICAL);
+  Serial.print("MOVED VERTICAL ");
+  Serial.println(thisCos - totalMovedY);
+  totalMovedY = totalMovedY + thisCos;
 }
